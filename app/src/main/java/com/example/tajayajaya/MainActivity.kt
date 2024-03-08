accdec9a924e948bde23779bbd02de9f79415a39
aswanazis <aswanazis9666@gmail.com>
2024-03-08 16:50:12 +0700

    logo facebook

--- app/src/main/java/com/example/tajayajaya/MainActivity.kt
+++ app/src/main/java/com/example/tajayajaya/MainActivity.kt
@@ -1,22 +1,23 @@
+package com.example.tajayajaya
+
 import android.content.Intent
 import android.os.Bundle
+import android.os.Parcel
+import android.os.Parcelable
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import com.example.tajayajaya.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Mendapatkan referensi ke tombol menggunakan findViewById()
-        val btn1: Button = findViewById(R.id.btn_1)
+        btnLoginListener()
+    }
 
-        // Menambahkan fungsi onClickListener ke tombol btn_1
-        btn1.setOnClickListener {
-            // Intent untuk memulai SecondActivity
-            val intent = Intent(this, com.example.tajayajaya.SecondActivity::class.java)
-            startActivity(intent)
+    private fun btnLoginListener() {
+        search_go_btn.setOnClickListener {
+
         }
     }
 }
